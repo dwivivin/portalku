4c457abf418dab5fe43dc1f463fb73aeca3c3bd5
Vivin Dwi A <dwivivinarianti@gmail.com>
2026-09-19 23:38:52 +0700

    praktik 1: jurnal beralih ke PostgreSQL

--- src/main/java/id/ac/polinema/lumajang/portalku/jurnal/JurnalRepository.java
+++ src/main/java/id/ac/polinema/lumajang/portalku/jurnal/JurnalRepository.java
@@ -1,17 +1,11 @@
 package id.ac.polinema.lumajang.portalku.jurnal;
 
-  
-import java.util.List; 
-import java.util.Optional;
-public interface JurnalRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-     List<jurnal> findAll(); 
-  
-    Optional<jurnal> findById(Integer id); 
-  
-    jurnal save(jurnal jurnal); 
-  
-    boolean deleteById(Integer id);
-
-    
+@Repository
+public interface JurnalRepository extends JpaRepository<jurnal, Integer> {
+    List<jurnal> findByPenerbit(String penerbit);
+    List<jurnal> findByJudulContainingIgnoreCase(String kataKunci);
 }
--- src/main/java/id/ac/polinema/lumajang/portalku/jurnal/jurnal.java
+++ src/main/java/id/ac/polinema/lumajang/portalku/jurnal/jurnal.java
@@ -1,19 +1,29 @@
 package id.ac.polinema.lumajang.portalku.jurnal;
 
-  
-import lombok.AllArgsConstructor; 
-import lombok.Getter; 
-import lombok.NoArgsConstructor; 
-import lombok.Setter; 
-  
-@Getter 
-@Setter 
-@NoArgsConstructor 
-@AllArgsConstructor 
-public class jurnal { 
-  
-    private Integer id; 
-    private String judul; 
-    private String penerbit; 
-    private Integer tahunTerbit; 
-} 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "jurnal")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class jurnal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String judul;
+
+    @Column(nullable = false, length = 100)
+    private String penerbit;
+
+    @Column(name = "tahun_terbit", nullable = false)
+    private Integer tahunTerbit;
+}
