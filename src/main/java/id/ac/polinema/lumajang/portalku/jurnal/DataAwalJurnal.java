0c9b5257be67c03342814eb67fd95f6af21df687
Vivin Dwi A <dwivivinarianti@gmail.com>
2026-09-20 20:28:47 +0700

    praktik 1: jurnal beralih ke PostgreSQL

---
+++ src/main/java/id/ac/polinema/lumajang/portalku/jurnal/DataAwalJurnal.java
@@ -0,0 +1,24 @@
+package id.ac.polinema.lumajang.portalku.jurnal;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("dev") // hanya berjalan pada profil dev
+@RequiredArgsConstructor
+public class DataAwalJurnal implements CommandLineRunner {
+
+    private final JurnalRepository jurnalRepository;
+
+    @Override
+    public void run(String... args) {
+        if (jurnalRepository.count() > 0) {
+            return; // jangan menggandakan data
+        }
+        jurnalRepository.save(new jurnal(null, "JISEBI", "Universitas Airlangga", 2015));
+        jurnalRepository.save(
+            new jurnal(null, "Jurnal Informatika Polinema", "Politeknik Negeri Malang", 2015));
+    }
+}
